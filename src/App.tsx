020ffc3baa2776ac2127bd809b6e9349bb6e4d7d
Damaris Avila <damarisavilaf@gmail.com>
2023-09-22 17:00:12 -0300

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import Counter from "./Components/Counter";
+import Usuario from "./Components/Usuario";
+
 function App() {
   return (
    <>
     <h1>React + TypeScript</h1>
+    <hr/>
+    <Counter />
+    <Usuario />
    </>
+  
   );
 }
 
